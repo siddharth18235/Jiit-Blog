f3ab60f6372fd20d435168e6235f8df83bc80275
Om Agarwal <omsr.agarwal@gmail.com>
2022-09-21 00:07:39 +0530

    name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Highlight } from "@material-ui/icons";
 
 function Header () {
-    return <header><h1><Highlight/> Keeper</h1></header>
+    return <header><h1><Highlight/> Jiit-Now</h1></header>
 };
 
 export default Header;
